4bc1ba13a95cade2f916e8440c6c8fa54f321279
Javier Doñoro <javidogar07051@gmail.com>
2026-09-16 12:56:40 +0200

    Añadido NavBar interactuable con boton de cambio de modo de color y añadido el boton de contador de cliks

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log("¡Hola DWEC! Mi taller ya funciona.");
+
+const themeToggle = document.getElementById("themeToggle");
+const contadorBtn = document.getElementById("contadorBtn");
+const contador = document.getElementById("contador");
+let cuenta = 0;
+
+if (themeToggle) {
+  themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    const isLightMode = document.body.classList.contains("light-mode");
+    themeToggle.textContent = isLightMode ? "Modo oscuro" : "Modo claro";
+  });
+}
+
+if (contadorBtn && contador) {
+  contadorBtn.addEventListener("click", () => {
+    cuenta++;
+    contador.textContent = cuenta;
+  });
+}
